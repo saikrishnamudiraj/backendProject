c667934e247a15cf1b141110083c53507a9a14f2
saikrishnamudiraj <saikrishna123.mudiraj@gmail.com>
2024-11-28 21:30:39 +0530

    commit1

---
+++ src/main/java/org/example/productcreation/ProductCreationApplication.java
@@ -0,0 +1,13 @@
+package org.example.productcreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCreationApplication.class, args);
+	}
+
+}
